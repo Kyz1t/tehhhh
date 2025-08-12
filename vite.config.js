dd9a8b5be19929a0364c88af65a8399bcd9b5be5
Kyz1t <baium232@gmail.com>
2025-08-12 15:51:17 +0300

    Fix all project problems and setup proper development environment

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,24 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
+  publicDir: 'images',
   server: {
     port: 8000,
-    host: true
+    host: true,
+    open: false
   },
   preview: {
     port: 8000,
-    host: true
+    host: true,
+    open: false
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 })
